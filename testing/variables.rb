d615cf60fb5c6e096ac44fe57a41bd76f9c862a3
Buronn <fernando.buron@mail.udp.cl>
2021-11-01 17:14:38 -0300

    test

--- testing/variables.rb
+++ testing/variables.rb
@@ -1,22 +1,20 @@
-class Buron
-  def initialize(name, second) #constructor
-    @name = name
-    @second = second
-  end
-  def name #getter
-    @name
-  end
-  def second #getter
-    @second
+require 'httparty'
+require 'json'
+require 'time'
+response = HTTParty.get('https://www.buda.com/api/v2/markets')
+ayer = Time.now.to_i-86400
+data = JSON.parse(response.body) if response.code == 200
+data['markets'].each do |child|
+  puts child['id']
+  response = HTTParty.get('https://www.buda.com/api/v2/markets/'+child['id']+'/trades')#?timestamp='+ayer.to_s+'&limit=100')
+  data2 = JSON.parse(response.body) if response.code == 200
+  max_of_day = 0
+  data2['trades']['entries'].each do |child2|
+    
+    if child2[0].to_i > ayer # compara si el timestamp es mayor al de ayer, o sea, si la transacción se hizo hace 24hrs
+      child2[1].to_f > max_of_day ? max_of_day = child2[1].to_f : max_of_day = max_of_day # busca la mayor transacción del día
     end
-    def name=(name) #setter
-    @name = name
-
   end
-  
+  puts 'max of the day ' + max_of_day.to_s + 'BTC' 
+  puts "\n"
 end
-
-buron = Buron.new("buron", "PUTA")
-puts buron.name
-buron.name = "perkinazo"
-puts buron.name + " is a good person" + " and " + buron.second + " is a bad person"
